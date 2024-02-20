2e9715d7a6a9f74ffb1057a229c7d5669d6dc8ce
mashca <mashca@Xubuntu.myguest.virtualbox.org>
2024-02-20 16:03:35 +0000

    fully tested unhappy path

--- server/app.js
+++ server/app.js
@@ -8,13 +8,15 @@ app.get('/api', getEndpoints);
 
 app.get('/api/topics', getTopics);
 
-app.get('/api/articles/:article_id', getArticleById)
+app.get('/api/articles/:article_id', getArticleById);
 
 app.all('/*', (req, res, next)=> {
-    res.status(404).send({msg: 'Path not found'})
+    res.status(404).send({msg: 'Path not found'});
   });
 
 app.use((err, req, res, next) => {
-res.status(500).send({ msg: 'internal server error' })
+    if(err.status && err.msg) res.status(err.status).send({msg: err.msg});
+    if(err.code = 23502) res.status(400).send({msg:'Bad request'});
+    res.status(500).send({ msg: 'internal server error' });
 })
 module.exports = app;
--- server/models/artciles-modules.js
+++ server/models/artciles-modules.js
@@ -4,6 +4,12 @@ exports.fetchArticleById = (article_id) => {
     return db
     .query('SELECT * FROM articles WHERE article_id = $1;', [article_id])
     .then((result) => {
-        return result.rows[0]
+        const article = result.rows[0];
+        if(!article) {
+        return Promise.reject({
+            status: 404,
+            msg: `No article found for article_id: ${article_id}`,
+          })};
+        return result.rows[0];
     });
 };
