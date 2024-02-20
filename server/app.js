e3e68381cec97cb228af63752e41b75b1ad4c464
mashca <mashca@Xubuntu.myguest.virtualbox.org>
2024-02-20 13:46:15 +0000

    completed task two

---
+++ server/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const { getTopics } = require('./controllers/topics-controllers');
+const app = express();
+
+app.get('/api/topics', getTopics);
+
+app.all('/*', (req, res, next)=> {
+    res.status(404).send({msg: 'Path not found'})
+  });
+
+app.use((err, req, res, next) => {
+res.status(500).send({ msg: 'internal server error' })
+})
+module.exports = app;
