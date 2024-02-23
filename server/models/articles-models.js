c6404f17ff28831e7070e49aef3b796c5f384d58
mashca <mashca@Xubuntu.myguest.virtualbox.org>
2024-02-23 12:27:59 +0000

    fixed issue of Promise.all concerrency problems which caused tests to occasionally fail

--- server/models/articles-models.js
+++ server/models/articles-models.js
@@ -52,7 +52,6 @@ exports.updateVotes = (newVotes, article_id) => {
          RETURNING *`, 
          [newVotes.inc_votes, article_id])
          .then((result) => {
-            if (result.rows.length === 0) {return Promise.reject({ status: 400, msg: 'bad request' })};
             return result.rows[0];
          });
 };
