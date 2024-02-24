77760982051ed65f01263adf2b998b93205ab89f
mashca <mashca@Xubuntu.myguest.virtualbox.org>
2024-02-24 00:40:22 +0000

    updated test wording

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -55,14 +55,18 @@ describe('/api/articles/:article_id', () => {
       return request(app)
       .get('/api/articles/1')
       .expect(200)
-      .then(({body}) => {
-        expect(body.article.title).toBe("Living in the shadow of a great man");
-        expect(body.article.topic).toBe("mitch");
-        expect(body.article.author).toBe( "butter_bridge");
-        expect(body.article.body).toBe( "I find this existence challenging");
-        expect(body.article).toHaveProperty("created_at");
-        expect(body.article.votes).toBe(100);
-        expect(body.article.article_img_url).toBe("https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700");
+      .then(({body : {article}}) => {
+        expect(article).toMatchObject({
+          author: "butter_bridge",
+          title: "Living in the shadow of a great man",
+          article_id: 1,
+          body: "I find this existence challenging",
+          topic: "mitch",
+          created_at: expect.any(String),
+          votes: 100,
+          article_img_url:
+            "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
+        });
       });
     });
     test("GET:200 responce includes a comment_count property", () => {
