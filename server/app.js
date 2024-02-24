664f99b19aa3491b9f65ddf2a852f7f83420ce6a
mashca <mashca@Xubuntu.myguest.virtualbox.org>
2024-02-24 00:23:11 +0000

    completed users router

--- server/app.js
+++ server/app.js
@@ -2,13 +2,12 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
-const { deleteComment } = require('./controllers/comments-controllers');
+
 const { customErrors, psqlErrors, serverErrors } = require('./controllers/error-controllers');
 const apiRouter = require('./routers/api-router');
 
 app.use('/api', apiRouter);
 
-app.delete('/api/comments/:comment_id', deleteComment);
 
 app.all('/*', (req, res, next)=> {
     res.status(404).send({msg: 'Path not found'}); //change to error handling controller
--- server/routers/api-router.js
+++ server/routers/api-router.js
@@ -2,6 +2,7 @@ const articlesRouter = require('./articles-router');
 const { getEndpoints } = require('../controllers/endpoint-controller');
 const topicsRouter = require('./topics-router');
 const usersRouter = require('./users-router');
+const commentsRouter = require('./comments-router');
 
 const apiRouter = require('express').Router();
 
@@ -10,5 +11,6 @@ apiRouter.get('/', getEndpoints);
 apiRouter.use('/topics', topicsRouter);
 apiRouter.use('/articles', articlesRouter);
 apiRouter.use('/users', usersRouter);
+apiRouter.use('/comments', commentsRouter);
 
 module.exports = apiRouter;
